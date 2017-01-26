9a7ff1aad7129aa8800fd637e84620a04b0fca2f
Bartosz Kryza <bkryza@gmail.com>
2017-01-26 22:03:31 +0100

    Added simple test

--- Formula/oneclient.rb
+++ Formula/oneclient.rb
@@ -41,7 +41,7 @@ class Oneclient < Formula
   def install
     ENV["PKG_CONFIG_PATH"]="/usr/local/opt/nss/lib/pkgconfig"
     ENV["DESTDIR"]="#{prefix}"
-    system "make", "release", "WITH_COVERAGE=OFF", "WITH_CEPH=OFF", 
+    system "make", "release", "WITH_COVERAGE=OFF", "WITH_CEPH=OFF", \
            "WITH_S3=OFF ", "WITH_SWIFT=OFF", "WITH_OPENSSL=OFF"
     system "make", "install"
   end
