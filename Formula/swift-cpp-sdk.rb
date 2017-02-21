693202502ec23fb59fd289e36b675183659a2b2a
Bartosz Kryza <bkryza@gmail.com>
2017-02-21 20:06:55 +0100

    Changed Swift_CPP_SDK repo from bkryza to onedata

--- Formula/swift-cpp-sdk.rb
+++ Formula/swift-cpp-sdk.rb
@@ -1,7 +1,7 @@
 class SwiftCppSdk < Formula
   desc "Openstack Swift C++ client ported to OSX"
   homepage "onedata.org"
-  url "https://github.com/bkryza/Swift_CPP_SDK.git", :branch => "feature/osx-port"
+  url "https://github.com/onedata/Swift_CPP_SDK.git", :branch => "feature/osx-port"
 
   def install
     system "make"
